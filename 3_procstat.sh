4ce69b2344441841d9b5c3a0133ac69809d07c35
Inryatt <inrymail@gmail.com>
2020-11-27 10:51:40 +0000

    reverted back to getting PID from /proc/, storing in PID array

--- 3_procstat.sh
+++ 3_procstat.sh
@@ -7,15 +7,13 @@ if [[ $# -lt 1 ]]; then
 	exit 1
 fi
 
-
-
 s=${@: -1}
 #echo .$s. is  s
 regexNum="^[0-9]+$"
-if [[ "$s" =~ $regexNum  ]] ;  then
-	
+if [[ "$s" =~ $regexNum ]]; then
+
 	echo
-	else
+else
 	echo "Error: Missing Arguments -O intervalo de tempo têm de ser o último argumento."
 	exit 1
 fi
@@ -24,137 +22,155 @@ echo "${options}"
 #echo "intervalo em segundos: $s"
 while getopts "c:s:e:u:wmtdr" options; do
 	case "${options}" in
-		c)
-			#WIP -- filtrar o nome DOS PROCESSOS (COMM) por REGEX
-			
-			if [[ $# -lt 3 ]];then
-				echo "Error-Missing Argument! Pass an regex after -c !"
-			else
-				fiter_regex=$OPTARG
-				echo "Opção c ainda não implementada - WIP"			
-			
-			fi
-			
-		;;
-	
-		s)
-			#WIP -- Filtrar Data Mínima
-			if [[ $# -lt 3 ]];then
-				echo "Error-Missing Argument! Pass the minimum date after -c !"
-			else
-				MIN_DATE=$OPTARG
-				echo "Opção s ainda não implementada - WIP"			
-				#if [[ ${OPTARG} ]] check date is formatted right and if theres a max date, its inferior to it
-			fi
+	c)
+		#WIP -- filtrar o nome DOS PROCESSOS (COMM) por REGEX
+
+		if [[ $# -lt 3 ]]; then
+			echo "Error-Missing Argument! Pass an regex after -c !"
+		else
+			fiter_regex=$OPTARG
+			echo "Opção c ainda não implementada - WIP"
+
+		fi
+
 		;;
 
-		e)
-			#WIP -- filtrar Data Máxima
-			if [[ $# -lt 3 ]];then
-					echo "Error-Missing Argument! Pass the maximum date after -c !"
-				else
-					MAX_DATE=$OPTARG
-					echo "Opção e ainda não implementada - WIP"			
-					#if [[ ${OPTARG} ]] check date is formatted right and if theres a min date, its superior to it
-				fi
+	s)
+		#WIP -- Filtrar Data Mínima
+		if [[ $# -lt 3 ]]; then
+			echo "Error-Missing Argument! Pass the minimum date after -c !"
+		else
+			MIN_DATE=$OPTARG
+			echo "Opção s ainda não implementada - WIP"
+			#if [[ ${OPTARG} ]] check date is formatted right and if theres a max date, its inferior to it
+		fi
 		;;
 
-		u)	
-			#WIP -- filtrar por nome de utilizador
-			
-			if [[ $# -lt 3 ]];then
-				echo "Error-Missing Argument! Pass an username after -c !"
-			else
-				NAME=$OPTARG
-				echo "Opção u ainda não implementada - WIP"			
-				#if [[ ${OPTARG} ]] check if user exists
-			fi
+	e)
+		#WIP -- filtrar Data Máxima
+		if [[ $# -lt 3 ]]; then
+			echo "Error-Missing Argument! Pass the maximum date after -c !"
+		else
+			MAX_DATE=$OPTARG
+			echo "Opção e ainda não implementada - WIP"
+			#if [[ ${OPTARG} ]] check date is formatted right and if theres a min date, its superior to it
+		fi
 		;;
 
-		w)
-			echo "w pressed"
-			if [[ $sortOpt == 0 ]]; then
-				sortOpt=1
-				#WIP -- sort on MEMup
-			    echo "Sort Opção w ainda não implementada - WIP"
-			else
-				echo "Error-Use apenas uma das seguintes opções: m t d w"
-				exit 1
-			fi
+	u)
+		#WIP -- filtrar por nome de utilizador
+
+		if [[ $# -lt 3 ]]; then
+			echo "Error-Missing Argument! Pass an username after -c !"
+		else
+			NAME=$OPTARG
+			echo "Opção u ainda não implementada - WIP"
+			#if [[ ${OPTARG} ]] check if user exists
+		fi
 		;;
 
-		m)
-			echo "m pressed"
-
-			if [[ $sortOpt == 0 ]]; then
-				sortOpt=1
-				#WIP -- sort on RATE
-			    echo "Sort Opção m ainda não implementada - WIP"
-			else
-				echo "Error-Use apenas uma das seguintes opções: m t d w"
-				exit 1
-			fi
-			
-		;;
-
-		t)
-			echo "t pressed"
-			if [[ $sortOpt == 0 ]]; then
-				sortOpt=1
-				#WIP -- sort on RSSup
-			    echo "Sort Opção m ainda não implementada - WIP"
-			else
-				echo "Error-Use apenas uma das seguintes opções: m t d w"
-				exit 1
-			fi
-		;;
-
-		d)
-			echo "d pressed"
-
-			if [[ $sortOpt == 0 ]]; then
-				sortOpt=1
-				#WIP -- sort on RATERup
-			    echo "Sort Opção m ainda não implementada - WIP"
-			else
-				echo "Error-Use apenas uma das seguintes opções: m t d w"
-				exit 1
-			fi
-		;;
-		
-		r)
-			echo "t pressed"
-			if [[ $sortOpt == 1 ]]; then
-				#WIP -- REVERSE SORT
-				echo "Opção c ainda não implementada - WIP"
-			else
-				echo "Error-Tem de usar uma opção destas opções também: m t d w"
-				exit 1
-			fi
-		;;
-
-		:)
-			
-			echo "Erro- ${OPTARG} has missing argument!"
+	w)
+		echo "w pressed"
+		if [[ $sortOpt == 0 ]]; then
+			sortOpt=1
+			#WIP -- sort on MEMup
+			echo "Sort Opção w ainda não implementada - WIP"
+		else
+			echo "Error-Use apenas uma das seguintes opções: m t d w"
 			exit 1
+		fi
 		;;
 
-		*)
-			echo "ERRO-Opção Inválida;" #REPLACE WITH STDERR? OR EQUIV EM BASH
+	m)
+		echo "m pressed"
+
+		if [[ $sortOpt == 0 ]]; then
+			sortOpt=1
+			#WIP -- sort on RATE
+			echo "Sort Opção m ainda não implementada - WIP"
+		else
+			echo "Error-Use apenas uma das seguintes opções: m t d w"
 			exit 1
+		fi
+
+		;;
+
+	t)
+		echo "t pressed"
+		if [[ $sortOpt == 0 ]]; then
+			sortOpt=1
+			#WIP -- sort on RSSup
+			echo "Sort Opção m ainda não implementada - WIP"
+		else
+			echo "Error-Use apenas uma das seguintes opções: m t d w"
+			exit 1
+		fi
+		;;
+
+	d)
+		echo "d pressed"
+
+		if [[ $sortOpt == 0 ]]; then
+			sortOpt=1
+			#WIP -- sort on RATERup
+			echo "Sort Opção m ainda não implementada - WIP"
+		else
+			echo "Error-Use apenas uma das seguintes opções: m t d w"
+			exit 1
+		fi
+		;;
+
+	r)
+		echo "t pressed"
+		if [[ $sortOpt == 1 ]]; then
+			#WIP -- REVERSE SORT
+			echo "Opção c ainda não implementada - WIP"
+		else
+			echo "Error-Tem de usar uma opção destas opções também: m t d w"
+			exit 1
+		fi
+		;;
+
+	:)
+
+		echo "Erro- ${OPTARG} has missing argument!"
+		exit 1
+		;;
+
+	*)
+		echo "ERRO-Opção Inválida;" #REPLACE WITH STDERR? OR EQUIV EM BASH
+		exit 1
 		;;
 	esac
 done
 
 #what to do if only time is passed to procstat
 if [[ $# == 1 ]]; then
-	pids=($(ps -au | awk '{ print $2 } ' | tail +2)	)
-	echo "pids= ${pids[@]}"	
+	#pids=($(ps -au | awk '{ print $2 } ' | tail +2)	)
+
+	PIDtmp=($(ls /proc/ -v | grep '[0-9]'))
+
+	#echo  "${PIDtmp[@]}"
+	#echo "Len= ${#PIDtmp[@]}"
+	for ((i = 0; i < ${#PIDtmp[@]}; i++)); do
+		#	echo "i= $i"
+		#	echo  ${PIDtmp[i]}
+		PIDarr[${PIDtmp[i]}]=${PIDtmp[i]} #WORKS -- PIDarr[PID] contem o valor de PID
+	done
+
+	for el in ${PIDarr[@]}; do
+		if [[ -f "/proc/$el/comm" ]]; then
+			cat /proc/$el/comm
+		fi
+	done
+	#pids=($(ps -e | awk '{print $1}' | tail +1)) get pids from ps - works well
+	#echo "pids= ${pids[@]}"
 	#exit 0
 fi
 
-for el in  ${pids[@]};do
-	
-	cat /proc/$el/comm
-done
-
+#for el in ${pids[@]}; do
+#	if [[ -f "/proc/$el/comm" ]]; then
+#		cat /proc/$el/comm
+#	fi
+#
+#done
