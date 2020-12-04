7cfe713046ff52ac9eaf709f93077acc1c650723
Diogo <diogo.mo.monteiro@ua.pt>
2020-12-04 21:10:08 +0000

    sorts, limite de processos, melhorias na validação de argumentos, melhoria na leitura inicial de PIDs

--- 3_procstat.sh
+++ 3_procstat.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-sortOpt=0
+sortCol=2
+sortRev=""		# "-r" para reverse, "" para normal
+numericSort=""	# "-n" para sort numérico, "" para alfabético
+tableMax=-1
 
-if [[ $# -lt 1 ]]; then
-	echo "Missing Arguments--Include at least the time interval"
-	exit 1
-fi
-
-s=${@: -1}
-#echo .$s. is  s
-regexNum="^[0-9]+$"
-if [[ "$s" =~ $regexNum ]]; then
-
-	:
-else
-	echo "Error: Missing Arguments -O intervalo de tempo têm de ser o último argumento."
-	exit 1
-fi
+#############  get PIDs  #############
 
 pids=($(ls /proc/ -v | grep '[0-9]'))
 
 for ((el = 0; el < ${#pids[@]}; el++)); do
-	if [[ $(cat /proc/${pids[$el]}/status 2>/dev/null | grep VmSize | awk '{print $2}') != "" ]] && [[ $(cat /proc/${pids[$el]}/status 2>/dev/null | grep VmRSS | awk '{print $2}') != " " ]] && [[ $(cat /proc/${pids[$el]}/io 2>/dev/null | grep wchar | awk '{print $2}') != " " ]] && [[ $(cat /proc/${pids[$el]}/io 2>/dev/null | grep rchar | awk '{print $2}') != " " ]] && [[ $"/proc/${pids[$el]}/status" != " " ]] && [[ -f "/proc/$el/comm" ]] && [[ -f "/proc/$el/io" ]] && [[ -f "/proc/$el/status" ]]; then
+	# verficar se a informação do processo pode ser lida
+	if [[ $(cat /proc/${pids[$el]}/status 2>/dev/null) != "" ]] \
+		&& [[ $(cat /proc/${pids[$el]}/status 2>/dev/null) != "" ]] \
+		&& [[ $(cat /proc/${pids[$el]}/io 2>/dev/null) != "" ]] \
+		&& [[ $(cat /proc/${pids[$el]}/io 2>/dev/null) != "" ]]; then		# alguns dos checks tavam mal (faziam != " " em vez de != "")
+		#&& [[ $"/proc/${pids[$el]}/status" != " " ]] \
+		#&& [[ -f "/proc/$el/comm" ]] && [[ -f "/proc/$el/io" ]] \
+		#&& [[ -f "/proc/$el/status" ]]; then							# these 3 are redundant (i think?)
 		:
 	else
 		toUnset+=($el)
@@ -34,6 +30,7 @@ done
 
 unset toUnset
 
+# this might not be necessary with the current sorting method
 #To fix array indexes              			IMPORTANT!  gotta be repeated everytime after pids is altered! :( blame bash and its dumb arrays
 for el in ${pids[@]}; do
 	tmp_pids+=($el)
@@ -47,183 +44,147 @@ done
 unset tmp_pids
 
 
-#echo "intervalo em segundos: $s"
-while getopts "c:s:e:u:wmtdr" options; do
+#############  process options given  #############
+
+while getopts "c:s:e:u:p:wmtdr" options; do
 
 	case "${options}" in
 	c)
 		#WIP -- filtrar o nome DOS PROCESSOS (COMM) por REGEX
+		toUnset=()
+		filter_regex="$OPTARG"
+		#echo "DEBUG: $filter_regex = filterregex"
+		for ((el = 0; el < ${#pids[@]}; el++)); do
+			#echo " DEBUG  el= $el , pid=${pids[$el]} , comm= $(cat /proc/${pids[$el]}/comm 2>/dev/null)"
+			if ! [[ $(cat /proc/${pids[$el]}/comm 2>/dev/null) =~ $filter_regex ]]; then
+				#echo "DEBUG: REMOVED $(cat /proc/${pids[$el]}/comm 2>/dev/null) "
+				toUnset+=($el)
+			fi
+		done
 
-		if [[ $OPTARG == $s ]] || [[ $# -lt 3 ]]; then #IMPORTANTE! ISTO FOI A MELHOR MANEIRA QUE DESCOBRI DE FAZER COM QUE ./PROCSTAT -C 3 DESSE ERRO POR NAO PASSAR PROPER ARGUMENTO AO -C!!
-			echo "Error-Missing Argument! Pass an regex after -c !"
-			exit 1
-		else
-			toUnset=()
-			filter_regex="$OPTARG"
-			#echo "DEBUG: $filter_regex = filterregex"
-			for ((el = 0; el < ${#pids[@]}; el++)); do
-				echo " DEBUG  el= $el , pid=${pids[$el]} , comm= $(cat /proc/${pids[$el]}/comm 2>/dev/null)"
-				if ! [[ $(cat /proc/${pids[$el]}/comm 2>/dev/null) =~ $filter_regex ]]; then
-					echo "DEBUG: REMOVED $(cat /proc/${pids[$el]}/comm 2>/dev/null) "
-					toUnset+=($el)
-				fi
-			done
+		#echo to unset ${toUnset[@]}
+		for el in ${toUnset[@]}; do
+			#echo unsetted $el
+			unset -v 'pids[$el]'
+		done
+		unset toUnset
+		#To fix array indexes              			IMPORTANT!
+		for el in ${pids[@]}; do
+			tmp_pids+=($el)
+		done
+		unset pids
 
-			#echo to unset ${toUnset[@]}
-			for el in ${toUnset[@]}; do
-				#echo unsetted $el
-				unset -v 'pids[$el]'
-			done
-			unset toUnset
-			#To fix array indexes              			IMPORTANT!
-			for el in ${pids[@]}; do
-				tmp_pids+=($el)
-			done
-			unset pids
+		for el in ${tmp_pids[@]}; do
+			pids+=($el)
+		done
 
-			for el in ${tmp_pids[@]}; do
-				pids+=($el)
-			done
-
-			unset tmp_pids
-
-			#echo pids= ${pids[@]}
-			#echo "Opção c ainda não implementada - WIP" eyy it done
-			shift $((OPTIND - 1)) #not sure if very needed
-		fi
-
+		unset tmp_pids
 		;;
 
 	s)
 		#WIP -- Filtrar Data Mínima
-		if [[ $# -lt 3 ]]; then
-			echo "Error-Missing Argument! Pass the minimum date after -s !"
-			exit 1
-		else
-			MIN_DATE_t=$OPTARG
+		MIN_DATE_t=$OPTARG
 
-			MAX_DATE=$(date -d $MIN_DATE_t +%s)
-			echo "Opção s ainda não implementada - WIP"
-			#if [[ ${OPTARG} ]] check date is formatted right and if theres a max date, its inferior to it
-		fi
+		MAX_DATE=$(date -d $MIN_DATE_t +%s)
+		echo "Opção s ainda não implementada - WIP"
+		#if [[ ${OPTARG} ]] check date is formatted right and if theres a max date, its inferior to it
 		;;
 
 	e)
 		#WIP -- filtrar Data Máxima
-		if [[ $# -lt 3 ]]; then
-			echo "Error-Missing Argument! Pass the maximum date after -c !"
-		else
-			MAX_DATE=$OPTARG
-			echo "Opção e ainda não implementada - WIP"
-			#if [[ ${OPTARG} ]] check date is formatted right and if theres a min date, its superior to it
-		fi
+		MAX_DATE=$OPTARG
+		echo "Opção e ainda não implementada - WIP"
+		#if [[ ${OPTARG} ]] check date is formatted right and if theres a min date, its superior to it
 		;;
 
 	u)
-		#WIP -- filtrar por nome de utilizador
+		#WIP -- filtrar por nome de utilizador   --- this ain't workin :(
 
-		if [[ $OPTARG == $s ]] || [[ $# -lt 3 ]]; then #IMPORTANTE! ISTO FOI A MELHOR MANEIRA QUE DESCOBRI DE FAZER COM QUE ./PROCSTAT -C 3 DESSE ERRO POR NAO PASSAR PROPER ARGUMENTO AO -C!!
-			echo "Error-Missing Argument! Pass an regex after -c !"
-			exit 1
+		toUnset=()
+		filterUser="$OPTARG"
+
+		for ((el = 0; el < ${#pids[@]}; el++)); do
+
+			#	echo $(ps -p ${pids[$el]} -o user= ) user to find is $filterUser
+			if [[ "$(ps -p ${pids[$el]} -o user= 2>/dev/null)" != "$filterUser" ]]; then
+				toUnset+=($el)
+			fi
+		done
+
+		for el in ${toUnset[@]}; do
+			unset -v 'pids[$el]'
+		done
+
+		unset toUnset
+
+		#To fix array indexes              			IMPORTANT!
+		for el in ${pids[@]}; do
+			tmp_pids+=($el)
+		done
+		unset pids
+
+		for el in ${tmp_pids[@]}; do
+			pids+=($el)
+		done
+
+		unset tmp_pids
+		;;
+
+	p)
+		# limit number of processes displayed on table
+		regexNum="^[0-9]+$"
+		if [[ "$OPTARG" =~ $regexNum ]]; then
+			tableMax=$OPTARG
 		else
-			toUnset=()
-			filterUser="$OPTARG"
-
-			for ((el = 0; el < ${#pids[@]}; el++)); do
-
-				#	echo $(ps -p ${pids[$el]} -o user= ) user to find is $filterUser
-				if [[ "$(ps -p ${pids[$el]} -o user= 2>/dev/null)" != "$filterUser" ]]; then
-					toUnset+=($el)
-				fi
-			done
-
-			for el in ${toUnset[@]}; do
-				unset -v 'pids[$el]'
-			done
-
-			unset toUnset
-
-			#To fix array indexes              			IMPORTANT!
-			for el in ${pids[@]}; do
-				tmp_pids+=($el)
-			done
-			unset pids
-
-			for el in ${tmp_pids[@]}; do
-				pids+=($el)
-			done
-
-			unset tmp_pids
-			
-			shift $((OPTIND - 1)) #not sure if very needed
+			echo "Error: O número de processos é inválido"
+			exit 1
 		fi
 		;;
 
 	w)
-		echo "w pressed"
-		if [[ $sortOpt == 0 ]]; then
-			sortOpt=1
-			#WIP -- sort on MEMup
-			echo "Sort Opção w ainda não implementada - WIP"
-		else
-			echo "Error-Use apenas uma das seguintes opções: m t d w"
-			exit 1
+		# sort on RATEW
+		if [[ $sortCol -ne 2 ]]; then
+			echo "WARNING - mais que uma opção de sort foi dada, apenas a última será considerada"
 		fi
+		sortCol=9
+		numericSort="-n"
 		;;
 
 	m)
-		echo "m pressed"
-
-		if [[ $sortOpt == 0 ]]; then
-			sortOpt=1
-			#WIP -- sort on RATE
-			echo "Sort Opção m ainda não implementada - WIP"
-		else
-			echo "Error-Use apenas uma das seguintes opções: m t d w"
-			exit 1
+		# sort on MEM
+		if [[ $sortCol -ne 2 ]]; then
+			echo "WARNING - mais que uma opção de sort foi dada, apenas a última será considerada"
 		fi
-
+		sortCol=4
+		numericSort="-n"
 		;;
 
 	t)
-		echo "t pressed"
-		if [[ $sortOpt == 0 ]]; then
-			sortOpt=1
-			#WIP -- sort on RSSup
-			echo "Sort Opção m ainda não implementada - WIP"
-		else
-			echo "Error-Use apenas uma das seguintes opções: m t d w"
-			exit 1
+		# sort on RSS
+		if [[ $sortCol -ne 2 ]]; then
+			echo "WARNING - mais que uma opção de sort foi dada, apenas a última será considerada"
 		fi
+		sortCol=5
+		numericSort="-n"
 		;;
 
 	d)
-		echo "d pressed"
-
-		if [[ $sortOpt == 0 ]]; then
-			sortOpt=1
-			#WIP -- sort on RATERup
-			echo "Sort Opção m ainda não implementada - WIP"
-		else
-			echo "Error-Use apenas uma das seguintes opções: m t d w"
-			exit 1
+		# sort on RATER
+		if [[ $sortCol -ne 2 ]]; then
+			echo "WARNING - mais que uma opção de sort foi dada, apenas a última será considerada"
 		fi
+		sortCol=8
+		numericSort="-n"
 		;;
 
 	r)
-		echo "t pressed"
-		if [[ $sortOpt == 1 ]]; then
-			#WIP -- REVERSE SORT
-			echo "Opção c ainda não implementada - WIP"
-		else
-			echo "Error-Tem de usar uma opção destas opções também: m t d w"
-			exit 1
-		fi
+		# reverse sort order
+		sortRev="-r"
 		;;
 
 	:)
 
-		echo "Erro- ${OPTARG} has missing argument!"
+		echo "Erro- ${options} has missing argument!"
 		exit 1
 		;;
 
@@ -234,7 +195,27 @@ while getopts "c:s:e:u:wmtdr" options; do
 	esac
 done
 
-#pids=($(ps -au | awk '{ print $2 } ' | tail +2)	)
+shift $((OPTIND -1))	# remover argumentos opcionais processados, o argumento de tempo passa a ser $1
+
+#############  validate given time argument  #############
+
+if [[ $# -ne 1 ]]; then		# tem de haver exatamente 1 argumento não opcional
+	echo "Missing Arguments--Include the time interval"  # trocar isto por um usage?
+	exit 1
+fi
+
+s=$1
+#echo .$s. is  s
+regexNum="^[0-9]+$"
+if [[ "$s" =~ $regexNum ]]; then
+	:
+else
+	echo "Error: O intervalo de tempo é inválido"
+	exit 1
+fi
+
+
+#############  get info on processes  #############
 
 for el in ${pids[@]}; do
 	comm[$el]=$(cat /proc/$el/comm 2>/dev/null)
@@ -259,28 +240,21 @@ for el in ${pids[@]}; do
 	ratew[$el]=$(bc <<<"scale=2;( $newwrite - ${writeb[$el]})/$s")
 done
 
-#echo ${pids[@]}
-#pids=($(ps -e | awk '{print $1}' | tail +1)) get pids from ps - works well
-#echo "pids= ${pids[@]}"
-#exit 0
-#echo should be equal ${#PIDarr[@]} ${#comm[@]}
+#############  format data, sort and print table  #############
 
-echo "PID   COMM     USER   MEM    RSS READB WRITEB      RATER    RATEW     DATE    "
-for el in ${pids[@]}; do
-	printf "%10s %-15s %-10s %10s %20s %20s %20s %20s %20s %-20s \n" $el ${comm[$el]} ${user[$el]} ${vmsize[$el]} ${rss[$el]} ${readb[$el]} ${writeb[$el]} ${rater[$el]} ${ratew[$el]} "${datestart[$el]}"
-	#echo $el ${comm[$el]} ${user[$el]} ${vmsize[$el]} ${rss[$el]} ${readb[$el]} ${writeb[$el]} ${rater[$el]} ${ratew[$el]} ${datestart[$el]}
-done
-#
-#	echo debugging: ${pids[@]}
-#	for el in ${pids[@]}; do
-#		echo $el
-#	done
+if [[ $tableMax -eq -1 ]]; then
+	tableMax=${#pids[@]}
+fi
 
-#for el in ${pids[@]}; do
-#	if [[ -f "/proc/$el/comm" ]]; then
-#		cat /proc/$el/comm
-#	fi
-#
-#done 
+printf "%8s | %16s | %10s | %10s | %15s | %15s | %15s | %15s | %15s | %20s \n" \
+	"PID" "COMM" "USER" "MEM" "RSS" "READB" "WRITEB" "RATER" "RATEW" "DATE"
 
-#a
+# formatar a informação de cada processo em linhas e dar pipe para o sort
+for ((line = 0; line < ${#pids[@]}; line++)); do
+	el=${pids[$line]}
+	printf "%8s | %-16s | %-10s | %10s | %15s | %15s | %15s | %15s | %15s | %-20s \n" \
+		$el "${comm[$el]}" ${user[$el]} ${vmsize[$el]} ${rss[$el]} ${readb[$el]} ${writeb[$el]} ${rater[$el]} ${ratew[$el]} "${datestart[$el]}"
+done | sort -t "|" -k $sortCol,$sortCol $numericSort $sortRev | head -n $tableMax
+# -t: separador de colunas para o sort
+# -k: coluna por qual é feito o sort
+# de seguida é feito o pipe do sort para o comando head, que com a opção -n limita o número de linhas de output
